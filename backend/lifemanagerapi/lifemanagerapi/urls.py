bdd8ce4c1749e03d98f44e0fd4150443e6f300f5
Sakibul Islam <120340281+SI2105@users.noreply.github.com>
2026-04-05 18:21:16 +0100

    Admin urls added
    -Exposes Django admin panels for admin

--- backend/lifemanagerapi/lifemanagerapi/urls.py
+++ backend/lifemanagerapi/lifemanagerapi/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 
@@ -28,10 +29,11 @@ router.register(r"groups", views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+      path("admin/", admin.site.urls),
     path("api/v1/", include(router.urls)), #
     path("api/v1/auth/", include("users.urls")),
     path("api/v1/", include("tasks.urls")),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")), #Browsable API login
     path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/v1/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/v1/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
